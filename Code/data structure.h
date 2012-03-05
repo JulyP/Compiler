360c7c8002de238fc65b5287b450dfd2e105c128
JulyP <jpishchikova@gmail.com>
2012-03-05 21:32:34 +1100

    updated data structure.h

--- Code/data structure.h
+++ Code/data structure.h
@@ -7,57 +7,58 @@
 //ОБЩИЕ ОШИБКИ
 struct error			//узел стека, в котором хранятся ошибки
 {
-	int numOfError;	//номер ошибки
+	int numOfError;	    //номер ошибки
 	int typeOFError;	//тип ошибки (лексическая, синтаксическая, семантическая)
 	int numOfString;	//номер строки, в которой произошла ошибка
 	int position;		//позиция ошибки (в какой позиции в строке произошла ошибка)
-}*Errors;			//указатель нас тек ошибок
+}*Errors;			    //указатель на стек ошибок
 
 
 //ИЗ ЛЕКСИЧЕСКОГО АНАЛИЗАТОРА
 struct tokensFromScanner
 {
-	int token;		//номер токена (от 0 до 22)
+	int token;		    //номер токена (от 0 до 22)
 	int numToken;		//номер атрибута (от 1 до 39)
-    int value;		//номер в таблице имен или констант
+    int value;		    //номер в таблице имен или констант
 	int numString;		//номер строки
 	int position;  		//номер позиции
 	struct tokensFromScanner *next;	//указатель на следующий элемент
 };
-struct tokensFromScanner *tokenHead, *tokenTail;
+struct tokensFromScanner *tokenHead, *tokenTail; //Указатели на хвост и на голову очереди
 
-struct Name	//таблица имен
+struct Name	            //таблица имен
 {
-	char name[]; //имя
+	char name[];        //имя
     char kindtoken[];	//тип токена (идентификатор или ключевое слово)
-    char kindid[]; // тип идентификатора(int, string, float)
-    int value; // номер в таблице строковых и числовых констант
-} *TableOfName // таблица имен
+    char kindid[];      // тип идентификатора(int, string, float)
+    int value;          // номер в таблице строковых и числовых констант
+} *TableOfName          // таблица имен
 
-struct Constant		//таблица констант
+struct Constant		    //таблица констант
 {
-	int num ;	//длина строковой константы
-	char str[];	//строка
-}* TableOfConst // таблица строковых и числовых конcтант
+	int num ;	        //длина строковой константы
+	char str[];	        //строка
+}* TableOfConst         // таблица строковых и числовых конcтант
 
 //ИЗ СИТАНКСИЧЕСКОГО АНАЛИЗАТОРА
-struct node		//узел дерева
+struct node		        //узел дерева
 {
-	int token; 	//номер лексемы (см. таблицу лексем)
+	int token; 	        //номер лексемы (см. таблицу лексем)
+	int numToken;		//номер атрибута (от 1 до 39)
 	struct node *alpha[4];	//массив указателей на другие узлы дерева
 };
-struct node *Root;  //указатель на корень синтаксического дерева
+struct node *Root;      //указатель на корень синтаксического дерева
 
 //ИЗ СЕМАНТИЧЕСКОГО АНАЛИЗАТОРА
-struct Table_Identificators 	//таблица идентификаторов
+struct Table_Identificators 	    //таблица идентификаторов
 {
 	struct Table_Id  table[250];	// В блоке <= 250 идентификаторов
 	struct Table_Identificators  *Child[20];	|| В блоке <= 20 вложенных подблоков
 	struct Table_Identificators  *Parent;	// Указатель на родительский блок
-	unsigned int level;	// Уровень вложенности данного блока
+	unsigned int level;             // Уровень вложенности данного блока
 }
 
-struct Table_Id		//таблица Id
+struct Table_Id		    //таблица Id
 {
 	struct Name  *Name;	//Ссылка на таблицу имен;
 	struct Constant *Const;	// Ссылка на таблицу констант;
